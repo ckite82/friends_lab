5d43db8539bd58906289f7c3435b6d4c2d77e1a1
Emma Hack <ehack78@gmail.com>
2020-10-15 14:13:12 +0100

    all functions complete

--- starting_point/src/friends.py
+++ starting_point/src/friends.py
@@ -20,3 +20,27 @@ def remove_friend(dictionary, friend_name):
         if friend == friend_name:
             dictionary["friends"].remove(friend_name)
 
+def total_money(list):
+    total_money = 0
+    for person in list:
+        total_money += person["monies"]
+    return total_money
+
+
+def l_money(person_1, person_2, loan_amount):
+    person_1["monies"] -= loan_amount
+    person_2["monies"] += loan_amount
+
+def all_favourite_foods(people):
+    all_food = []
+    for person in people:
+        for item in person["favourites"]["snacks"]:
+            all_food.append(item)
+    return all_food
+
+def find_no_friendends(list):
+    no_mates = []
+    for person in list:
+        if len(person["friends"]) == 0:
+            no_mates.append(person)
+    return no_mates
