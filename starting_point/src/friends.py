909fe04c2f8332f9cb4d4fe75cf121fd1a39956c
Emma Hack <ehack78@gmail.com>
2020-10-15 12:18:45 +0100

    function_four

--- starting_point/src/friends.py
+++ starting_point/src/friends.py
@@ -5,7 +5,10 @@ def get_favourite_tv_show(dictionary):
     return dictionary["favourites"]["tv_show"]
 
 def likes_to_eat(dictionary, food):
-    # food_type = dictionary["favourites"] ["snacks"]
     for item in dictionary["favourites"] ["snacks"]:
         if item == food:
             return True
+    else:
+        return False
+
+
